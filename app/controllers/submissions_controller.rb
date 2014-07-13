a3d1e2403af85d8ef8585ed44ecf4a3a0ddba0ea
Aubrey Howell <simplyaubs@gmail.com>
2014-07-13 16:54:39 -0600

    User can create a see list of grateful submissions.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,22 @@
 class SubmissionsController < ApplicationController
 
   def index
-    
+    @submission = Submission.new
+    @submissions = Submission.all
+  end
+
+  def create
+    @submission = Submission.create(submission_params)
+    if @submission.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def submission_params
+    params.require(:submission).permit(:submission)
   end
 
 end
--- spec/features/submissions_spec.rb
+++ spec/features/submissions_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 feature 'Add submissions' do
-  scenario 'User can see Welcome to the Grateful Garden' do
+  scenario 'User can see and add submissions to the garden' do
     visit '/'
     expect(page).to have_content 'Welcome to the Grateful Garden'
+    fill_in 'submission[submission]', with: 'I\'m learning to program'
+    click_on 'I\'M GRATEFUL'
+    expect(page).to have_content 'I\'m learning to program'
   end
 end
